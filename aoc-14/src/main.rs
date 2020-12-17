d5cf63bf1b8699d20b741000c41d272252a613d5
Ben Sandwick <ben.sandwick@foxbytegames.co.uk>
2020-12-17 17:02:27 +0000

    Day 14 almost done

--- aoc-14/src/main.rs
+++ aoc-14/src/main.rs
@@ -1,14 +1,22 @@
 use std::env;
 use std::fs;
+use std::collections::HashMap;
 
 fn num_to_bin(num: isize) -> String {
-    format!("{:b}", num)
+    let mut result = format!("{:b}", num);
+    let pad = 36 - result.len();
+    for _ in 0 .. pad {
+        result = String::from("0") + &result;
+    }
+
+    result
 }
 
 fn bin_to_num(bin: &str) -> isize {
     isize::from_str_radix(bin, 2).expect("Could not turn binary string into number")
 }
 
+#[derive(Debug)]
 struct Mask {
     mask: Vec<char>
 }
@@ -33,14 +41,57 @@ impl Mask {
     }
 }
 
+#[derive(Debug)]
+enum Instruction {
+    MaskSet(Mask),
+    MemSet(isize, isize)
+}
+
 fn main() {
-    // let args: Vec<String> = env::args().collect();
+    let args: Vec<String> = env::args().collect();
 
-	// let filename = &args[1];
+	let filename = &args[1];
 
-	// let file_contents = fs::read_to_string(filename).expect("Error reading file");
-    // let lines: Vec<&str> = file_contents.lines().collect();
+	let file_contents = fs::read_to_string(filename).expect("Error reading file");
+    let lines: Vec<&str> = file_contents.lines().collect();
 
-    let mask = Mask::from("0X1");
-    println!("{}", mask.apply(4));
+    let mut instructions = Vec::new();
+
+    for line in lines.iter() {
+        if &line[..4] == "mask" {
+            let mask_str = (&line[7..]).to_owned();
+            let mask = Mask::from(&mask_str);
+            instructions.push(Instruction::MaskSet(mask));
+        } else {
+            let mut sqbrt_end = 0;
+            for (i, c) in line[4..].chars().enumerate() {
+                if c == ']' {
+                    sqbrt_end = i + 4;
+                    break;
+                }
+            }
+
+            let mem_addr = line[4 .. sqbrt_end].parse::<isize>().unwrap();
+            let mem_val = line[sqbrt_end + 4 ..].parse::<isize>().unwrap();
+            instructions.push(Instruction::MemSet(mem_addr, mem_val));
+        }
+    }
+
+    let mut mask = Mask::from("X");
+    let mut memory: HashMap<isize, isize> = HashMap::new();
+    for instruction in instructions {
+        match instruction {
+            Instruction::MaskSet(new_mask) => mask = new_mask,
+            Instruction::MemSet(addr, val) => { 
+                let masked_val = mask.apply(val);
+                memory.entry(addr).and_modify(|v| *v = masked_val).or_insert(masked_val); 
+            }
+        }
+    }
+
+    let mut result = 0;
+    for (_, v) in memory.iter() {
+        result += v;
+    }
+    println!("{}", result);
 }
